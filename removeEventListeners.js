b7b71975c1e445463d7d707a4d5ce30b77e57d0d
hardikgupta2003 <94279016+hardikgupta2003@users.noreply.github.com>
2023-06-10 14:02:58 +0530

    Update removeEventListeners.js

--- removeEventListeners.js
+++ removeEventListeners.js
@@ -1,1 +1,6 @@
+function EventListener(){
+    console.log(' i have clicked on the screen');
+}
 
+document.addEventListener('click',EventListener);
+document.removeEventListener('click',EventListener);
