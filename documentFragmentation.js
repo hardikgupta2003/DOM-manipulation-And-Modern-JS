3fff95c8583eb203ca91c84d80b1d419549d0de1
hardikgupta2003 <94279016+hardikgupta2003@users.noreply.github.com>
2023-06-12 22:35:42 +0530

    Create documentFragmentation.js

---
+++ documentFragmentation.js
@@ -0,0 +1,8 @@
+// document Fragmentation is used to minimize the reflows and repaints
+let fragment=document.createDocumentFragment();
+for(let i=1;i<=100;i++){
+    newElement=document.createElement('p');
+    newElement.textContent='this is para '+ i;
+    fragment.appendChild(newElement);
+}
+document.body.appendChild(fragment)
