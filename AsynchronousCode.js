0256fb3dc021b344f6f54165f9e8377376e67e7c
hardikgupta2003 <94279016+hardikgupta2003@users.noreply.github.com>
2023-06-13 13:50:04 +0530

    Update AsynchronousCode.js

--- AsynchronousCode.js
+++ AsynchronousCode.js
@@ -15,3 +15,23 @@ let myPromise1= new Promise(function(resolve,reject){
     // reject(new Error("error is back "))
 });
 console.log("this is the text before promise")
+
+
+// with then function 
+let waada1= new Promise(function(resolve,reject){
+    setTimeout(()=>{
+        console.log('setTimeout1 started');
+    },2000);
+    resolve('resolved')
+});
+
+let output=waada1.then(()=>{
+    let waada2=new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            console.log('setTimeout2 started');
+        },3000);
+        resolve("waada 2 resolved");
+    })
+    return waada2;
+})
+output.then((value)=> console.log(value));
